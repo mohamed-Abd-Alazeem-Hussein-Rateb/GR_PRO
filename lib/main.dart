448c46a373d1c0e2a68d6ae1ab3df832be5d9e31
mohamed-Abd-Alazeem-Hussein-Rateb <mohamedabdlazeem36@gmail.com>
2025-03-21 17:33:19 +0200

    creat fiearbase

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:firebase_core/firebase_core.dart'; // ✅ إضافة Firebase
 import 'package:grow/features/Home/data/service.dart';
 import 'package:grow/features/Home/presentation/manage/cubit/cubits_cubit.dart';
 import 'package:grow/features/splach/presentation/views/splach_view.dart';
+import 'firebase_options.dart'; // ✅ استيراد الإعدادات التي تم إنشاؤها تلقائيًا
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // ✅ التأكد من تهيئة Flutter قبل تشغيل Firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform, // ✅ تهيئة Firebase بناءً على المنصة
+  );
+
   runApp(
     DevicePreview(
       enabled: true, // تأكد من تفعيل المعاينة
@@ -20,7 +27,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context) => WeatherCubit(WeatherService())..getWeather(), // ✅ تعديل هنا
+      create: (context) => WeatherCubit(WeatherService())..getWeather(), // ✅ Cubit للطقس
       child: MaterialApp(
         builder: DevicePreview.appBuilder,
         debugShowCheckedModeBanner: false,
